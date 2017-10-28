e30458cdae8d72f63532d48b0919bcbb20941682
Administrator <123456>
2017-10-28 17:27:53 +0800

    ..

--- app/src/main/java/com/tsutsuku/artcollection/ui/mine/InviteActivity.java
+++ app/src/main/java/com/tsutsuku/artcollection/ui/mine/InviteActivity.java
@@ -70,9 +70,9 @@ public class InviteActivity extends BaseActivity {
                     oks.disableSSOWhenAuthorize();
 
                     // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
-                    oks.setTitle("艺术收藏网");
+                    oks.setTitle("好友给您送1000现金积分");
                     // text是分享文本，所有平台都需要这个字段
-                    oks.setText("艺术收藏网邀请好友送1000现金积分啦");
+                    oks.setText("好友邀请您加入艺术收藏网，今天的艺术品就是明天的古玩。");
 
                     // url仅在微信（包括好友和朋友圈）中使用
                     oks.setUrl(model.getList().getInvite_url() + model.getCode());
