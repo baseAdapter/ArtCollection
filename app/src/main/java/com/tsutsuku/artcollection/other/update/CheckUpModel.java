4660ee40fe074c32e8cceff99131714553a3be49
dengchao <584594403@qq.com>
2017-10-30 18:38:19 +0800

    更新

---
+++ app/src/main/java/com/tsutsuku/artcollection/other/update/CheckUpModel.java
@@ -0,0 +1,75 @@
+package com.tsutsuku.artcollection.other.update;
+
+import com.tsutsuku.artcollection.model.BaseModel;
+
+/**
+ * Created by DengChao on 2017/10/30.
+ */
+
+public class CheckUpModel extends BaseModel{
+
+    /**
+     * info : {"terminal_version":"1.2.1","content":"【新增】用户注册或购买商品可获得一定数量的积分\r\n【新增】积分兑换金币，金币可用于兑换商品或提现\r\n【新增】金币商城，用金币兑换你喜欢的礼品\r\n【新增】app推广活动，分享给你的好友赚取金币\r\n【新增】版本更新提示，让用户第一时间收到最棒更新\r\n【优化】全新注册，登录，找回密码界面，增强用户体验","status":"2","address":"https://www.2345.com/"}
+     */
+
+    private InfoBean info;
+
+    public InfoBean getInfo() {
+        return info;
+    }
+
+    public void setInfo(InfoBean info) {
+        this.info = info;
+    }
+
+    public static class InfoBean {
+        /**
+         * terminal_version : 1.2.1
+         * content : 【新增】用户注册或购买商品可获得一定数量的积分
+         【新增】积分兑换金币，金币可用于兑换商品或提现
+         【新增】金币商城，用金币兑换你喜欢的礼品
+         【新增】app推广活动，分享给你的好友赚取金币
+         【新增】版本更新提示，让用户第一时间收到最棒更新
+         【优化】全新注册，登录，找回密码界面，增强用户体验
+         * status : 2
+         * address : https://www.2345.com/
+         */
+
+        private String terminal_version;
+        private String content;
+        private String status;
+        private String address;
+
+        public String getTerminal_version() {
+            return terminal_version;
+        }
+
+        public void setTerminal_version(String terminal_version) {
+            this.terminal_version = terminal_version;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
+}
